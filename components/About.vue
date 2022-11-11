ab61bc7823238e2bfdb48ae3e67287c27aa61525
Jeffrey Spies <code@jeffspies.com>
2022-11-11 13:46:25 -0500

    update

--- components/About.vue
+++ components/About.vue
@@ -4,6 +4,9 @@
       <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium ">{{ title }}</h1>
         <p class="mb-8 leading-relaxed">{{ text }}</p>
+        <p class="text-center">
+          <strong>Press Inquiries </strong> Please <a href="mailto:press@221b.io" class="underline">email</a> TLCUA Lead Negotiator, Dr. Jeffrey Spies.
+        </p>
         <!-- <p class="mb-8 leading-relaxed">
           <a href="https://twitter.com/TexasLibUnited"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
           <a href="/feed.xml"><font-awesome-icon :icon="['fas', 'rss-square']" /></a>
