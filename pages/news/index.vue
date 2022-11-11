e30821b06c4c26662e71cca084c7d70f99c7c019
Jeffrey Spies <code@jeffspies.com>
2022-11-11 14:03:53 -0500

    update

---
+++ pages/news/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li>
+      <NuxtLink class="mr-5 hover:text-primaryBlue" :to="{ path: '/news/2020/08/texas-universities-join-forces', hash:'' }">
+        Texas Universities Join Forces to Negotiate Their Future
+      </NuxtLink>
+    </li>
+    <li>
+      <a href="https://www.utsystem.edu/news/2020/09/16/ut-system-joins-texas-library-coalition-improve-access-scientific-journals">
+        UT System joins Texas Library Coalition to improve access to scientific journals
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+  components: {},
+  data () {
+    return {
+      people: []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
