04c09105204a23543640bc8f56faed2d8d63def1
Jeffrey Spies <code@jeffspies.com>
2022-11-30 02:07:55 -0500

    icons

--- components/FollowUs.vue
+++ components/FollowUs.vue
@@ -3,24 +3,18 @@
       <div class="container grid grid-cols-2 text-center p-8 mx-auto">
         <div>
           <a
-            class="px-4 py-12"
+            class="px-4 py-12 text-2xl"
             href="https://twitter.com/TexasLibUnited"
           >
-            <font-awesome-icon
-              :icon="['fab', 'twitter']"
-              size="2x"
-              class="text-primaryBlue w-24 h-24"
-            />
-            <p class="text-2xl">Follow us on Twitter</p>
+            Follow us on Twitter
           </a>
         </div>
         <div>
           <a
-            class="px-4 py-12"
+            class="px-4 py-12 text-2xl"
             href="mailto:press@221b.io"
           >
-            <font-awesome-icon :icon="['far', 'envelope']" size="2x" class="text-primaryBlue w-24 h-24" />
-            <p class="text-2xl">Send us an email</p>
+            Send us an email
           </a>
         </div>
       </div>
