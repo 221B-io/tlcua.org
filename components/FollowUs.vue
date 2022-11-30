4d3aa0bda01bbbf928d061fd89c75a768e5b8548
Jeffrey Spies <code@jeffspies.com>
2022-11-30 01:43:16 -0500

    icons

--- components/FollowUs.vue
+++ components/FollowUs.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="text-primaryBlue body-font container px-5 pt-24 mx-auto items-top">
     <div class="">
-      <h1 class="title-font font-medium text-3xl">Stay In Touch</h1>
+      <h1 class="title-font font-medium text-3xl text-center">Stay In Touch</h1>
       <div class="container grid grid-cols-2 text-center p-8 mx-auto">
         <div class="px-4 py-12">
           <a
@@ -9,7 +9,7 @@
           >
             <font-awesome-icon
               :icon="['fab', 'twitter']"
-              size="4x"
+              size="2x"
               class="text-primaryBlue"
             />
             <p class="text-2xl">Follow us on Twitter</p>
@@ -19,7 +19,7 @@
           <a
             href="mailto:press@221b.io"
           >
-            <font-awesome-icon :icon="['far', 'envelope']" size="4x" class="text-primaryBlue" />
+            <font-awesome-icon :icon="['far', 'envelope']" size="2x" class="text-primaryBlue" />
             <p class="text-2xl">Send us an email</p>
           </a>
         </div>
