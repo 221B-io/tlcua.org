f329609119900f0306c81ccc5e615dd017c1a59e
Jeffrey Spies <code@jeffspies.com>
2022-11-30 01:59:11 -0500

    icons

--- components/FollowUs.vue
+++ components/FollowUs.vue
@@ -1,26 +1,24 @@
 <template>
   <section class="text-primaryBlue body-font container px-5 mt-24 mx-auto items-top border-t">
       <div class="container grid grid-cols-2 text-center p-8 mx-auto">
-        <div class="px-4 py-12">
-          <a
-            href="https://twitter.com/TexasLibUnited"
-          >
-            <font-awesome-icon
-              :icon="['fab', 'twitter']"
-              size="2x"
-              class="text-primaryBlue w-24 h-24"
-            />
-            <p class="text-2xl">Follow us on Twitter</p>
-          </a>
-        </div>
-        <div class="px-4 py-12">
-          <a
-            href="mailto:press@221b.io"
-          >
-            <font-awesome-icon :icon="['far', 'envelope']" size="2x" class="text-primaryBlue w-24 h-24" />
-            <p class="text-2xl">Send us an email</p>
-          </a>
-        </div>
+        <a
+          class="px-4 py-12"
+          href="https://twitter.com/TexasLibUnited"
+        >
+          <font-awesome-icon
+            :icon="['fab', 'twitter']"
+            size="2x"
+            class="text-primaryBlue w-24 h-24"
+          />
+          <p class="text-2xl">Follow us on Twitter</p>
+        </a>
+        <a
+          class="px-4 py-12"
+          href="mailto:press@221b.io"
+        >
+          <font-awesome-icon :icon="['far', 'envelope']" size="2x" class="text-primaryBlue w-24 h-24" />
+          <p class="text-2xl">Send us an email</p>
+        </a>
       </div>
   </section>
 </template>
